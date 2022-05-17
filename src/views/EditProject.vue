c5c11c68fca29c1cd5cfb791806e37c4d75dbd6b
jodhy12 <jodhy12@aol.com>
2022-05-17 21:37:42 +0700

    project-planner

---
+++ src/views/EditProject.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+    <label>Title: </label>
+    <input type="text" v-model="title" required>
+    <label>Details: </label>
+    <textarea v-model="details" required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data(){
+        return{
+            title : '',
+            details : '',
+            url: 'http://localhost:3000/projects/' + this.id
+        }
+    },
+    mounted(){
+        fetch(this.url)
+        .then(res => res.json())
+        .then(data => {
+            this.title = data.title
+            this.details = data.details
+        }).catch(err => console.log(err))
+    },
+
+    methods: {
+        handleSubmit(){
+            let project = {
+                title : this.title,
+                details : this.details
+            }
+            console.log(project)
+            fetch(this.url, {
+                method : 'PATCH',
+                headers : { 'Content-Type' : 'application/json' },
+                body : JSON.stringify(project)
+            }).then(() => this.$router.push('/'))
+            .catch(err => console.log(err))
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

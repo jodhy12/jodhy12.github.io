7654d95ace53755045008650fd69861d6695c12f
jodhy12 <jodhy12@aol.com>
2022-06-30 16:27:10 +0700

    API

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-module.exports = {
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-};
+
+})
